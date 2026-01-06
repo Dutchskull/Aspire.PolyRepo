fa29de76f69c2e8305bf164ba66ddb8f7696d1a4
SpeedyR10 <tiagors18@hotmail.com>
2026-01-06 16:03:25 +0000

    add 2 examples of passing environment variables to vite applications, ui now resembles previous api example

--- src/Dutchskull.Aspire.PolyRepo.Vite/src/App.tsx
+++ src/Dutchskull.Aspire.PolyRepo.Vite/src/App.tsx
@@ -1,10 +1,35 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+type Forecast = {
+  date: string,
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [forecasts, setForecasts] = useState<Forecast[]>([]);
+
+  const weatherApi = import.meta.env.VITE_WEATHER_API;
+
+  console.log(weatherApi);
+
+  useEffect(() => {
+    const fetchData = async (weatherApi: string) => {
+      const weather = await fetch(weatherApi);
+      console.log(weather);
+
+      const weatherJson = await weather.json();
+      console.log(weatherJson);
+
+      setForecasts(weatherJson);
+    };
+
+    fetchData(weatherApi);
+  }, [weatherApi]);
 
   return (
     <>
@@ -17,17 +42,38 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Mode: {import.meta.env.VITE_ENVIRONMENT_MODE}</h2>
+      <table>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Temp. (C)</th>
+              <th>Temp. (F)</th>
+              <th>Summary</th>
+            </tr>
+          </thead>
+          <tbody>
+            {(
+              forecasts ?? [
+                {
+                  date: "N/A",
+                  temperatureC: "",
+                  temperatureF: "",
+                  summary: "No forecasts",
+                },
+              ]
+            ).map((w) => {
+              return (
+                <tr>
+                  <td>{w.date}</td>
+                  <td>{w.temperatureC}</td>
+                  <td>{w.temperatureF}</td>
+                  <td>{w.summary}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
     </>
   )
 }
--- src/Dutchskull.Aspire.PolyRepo.Vite/vite.config.ts
+++ src/Dutchskull.Aspire.PolyRepo.Vite/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv, type ConfigEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }: ConfigEnv) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  console.log(env.services__apiservice__http__0)
+  return defineConfig({
+    define: {
+      plugins: [react()],
+      'import.meta.env.VITE_WEATHER_API': JSON.stringify(env.services__apiservice__http__0),
+    },
+  });
+};
